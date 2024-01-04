2069de60c1f599f9b555e4c78b9081c27bc42f4a
kacper <kacper.markowski2@teb.edu.pl>
2024-01-04 16:29:07 +0100

    podstawowe wyświetlenie zadania na stronie głównej

--- task_mngr_app/urls.py
+++ task_mngr_app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
-
+from .views import TaskView
 
 urlpatterns = [
-    path('', views.home, name="home"),
+    #path('', views.home, name="home"),
+    path('', TaskView.as_view(), name="task")
 ]
--- task_mngr_app/views.py
+++ task_mngr_app/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Task
 
 
-def home(request):
-    return render(request, 'home.html', {})
+# def home(request):
+#    return render(request, 'home.html', {})
+
+
+class TaskView(ListView):
+    model = Task
+    template_name = 'home.html'
